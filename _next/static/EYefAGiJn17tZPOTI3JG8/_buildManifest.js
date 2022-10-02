abc38d129fd0ef6bc75d3c4b9375672ea38143a8
happy2introvert <62071412+happy2introvert@users.noreply.github.com>
2022-10-02 11:31:56 +0530

    Add files via upload

---
+++ _next/static/EYefAGiJn17tZPOTI3JG8/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/e8c5e5aae26e7775.css","static/chunks/pages/index-a314cffc4e280b14.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
